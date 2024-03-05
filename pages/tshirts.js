3e3943552239af38fab7358cdfcd47ad5baa1134
Gaganjot <gaganjothans777@gmail.com>
2024-03-05 16:18:33 -0500

    Fetching products from database using getServerSideProps

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,186 +1,56 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import React from "react";
-
-const Tshirts = () => {
+import Product from "../models/Product";
+import mongoose from "mongoose";
+const Tshirts = ({ products }) => {
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            <div className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg">
-              <Link
-                href={"/product/wear-to-code"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://m.media-amazon.com/images/I/41VyuGAOQUL._AC_SX679_.jpg"
-                />
-              </Link>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">$16.00</p>
-              </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg">
-              <Link
-                href={"/product/wear-to-code"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://m.media-amazon.com/images/I/41VyuGAOQUL._AC_SX679_.jpg"
-                />
-              </Link>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Shooting Stars
-                </h2>
-                <p className="mt-1">$21.15</p>
-              </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg">
-              <Link
-                href={"/product/wear-to-code"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://m.media-amazon.com/images/I/41VyuGAOQUL._AC_SX679_.jpg"
-                />
-              </Link>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Neptune
-                </h2>
-                <p className="mt-1">$12.00</p>
-              </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg">
-              <Link
-                href={"/product/wear-to-code"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://m.media-amazon.com/images/I/41VyuGAOQUL._AC_SX679_.jpg"
-                />
-              </Link>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The 400 Blows
-                </h2>
-                <p className="mt-1">$18.40</p>
-              </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg">
-              <Link
-                href={"/product/wear-to-code"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://m.media-amazon.com/images/I/41VyuGAOQUL._AC_SX679_.jpg"
-                />
-              </Link>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">$16.00</p>
-              </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg">
-              <Link
-                href={"/product/wear-to-code"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://m.media-amazon.com/images/I/41VyuGAOQUL._AC_SX679_.jpg"
-                />
-              </Link>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Shooting Stars
-                </h2>
-                <p className="mt-1">$21.15</p>
-              </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg">
-              <Link
-                href={"/product/wear-to-code"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://m.media-amazon.com/images/I/41VyuGAOQUL._AC_SX679_.jpg"
-                />
-              </Link>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Neptune
-                </h2>
-                <p className="mt-1">$12.00</p>
-              </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg">
-              <Link
-                href={"/product/wear-to-code"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://m.media-amazon.com/images/I/41VyuGAOQUL._AC_SX679_.jpg"
-                />
-              </Link>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The 400 Blows
-                </h2>
-                <p className="mt-1">$18.40</p>
-              </div>
-            </div>
+            {products.map((item) => {
+              return (
+                <div
+                  key={item._id}
+                  className="lg:w-1/4 md:w-1/2 p-8 w-full shadow-lg "
+                >
+                  <Link
+                    href={`/product/${item.slug}`}
+                    className="block relative rounded overflow-hidden"
+                  >
+                    <img
+                      alt="ecommerce"
+                      className="object-cover object-center w-full h-[40vh] block"
+                      src={item.img}
+                    />
+                  </Link>
+                  <div className="mt-4">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                      {item.category}
+                    </h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">
+                      {item.title}
+                    </h2>
+                    <p className="mt-1">${item.price}</p>
+                    <p className="mt-1">{item.size}</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
     </div>
   );
 };
-
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let products = await Product.find({ category: "tshirt" });
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) },
+  };
+}
 export default Tshirts;
