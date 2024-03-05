f2641973e5cf97b5baca1810345fad129331fd3e
Gaganjot <gaganjothans777@gmail.com>
2024-03-05 12:06:54 -0500

    Models created using mongoose Schema

---
+++ pages/api/getproducts.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Product from "../../models/Product";
+import connectDb from "../../middleware/mongoose";
+
+export default function handler(req, res) {
+  res.status(200).json({ name: "John Doe" });
+}
