92b80322a50b5392d7aff09ffaaa8cef18432fd5
Gaganjot <gaganjothans777@gmail.com>
2024-03-05 13:35:19 -0500

    APIs created for database(add, update , getProducts functionality added)

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -2,6 +2,8 @@
 import Product from "../../models/Product";
 import connectDb from "../../middleware/mongoose";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
+const handler = async (req, res) => {
+  let products = await Product.find();
+  res.status(200).json({ products });
+};
+export default connectDb(handler);
